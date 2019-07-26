22ee30822078881f6632ff5c974aded5fc2f93cc
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-26 02:19:42 -0300

    feat: added private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
@@ -16,11 +17,13 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/details" component={Details} />
-      <Route path="/new" component={New} />
-      <Route path="/edit/:id" component={Edit} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/details" component={Details} isPrivate />
+      <Route path="/new" component={New} isPrivate />
+      <Route path="/edit/:id" component={Edit} isPrivate />
+
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
