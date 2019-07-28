8db2d1bd69dd9b8be32cb5af1f0f3ac91f173bdb
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-28 01:59:40 -0300

    feat: added unform validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="MeetApp" />
 
-      <form action="">
-        <input type="email" placeholder="Insert your e-mail" />
-        <input type="password" placeholder="Your secret password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Insert your e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
 
         <button type="submit">Login</button>
         <Link to="/">Register for free!</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="MeetApp" />
 
-      <form action="">
-        <input placeholder="Your full name" />
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your secret password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Your full name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
 
         <button type="submit">Go for Meetups!</button>
         <Link to="/login">I'm already registered</Link>
-      </form>
+      </Form>
     </>
   );
 }
