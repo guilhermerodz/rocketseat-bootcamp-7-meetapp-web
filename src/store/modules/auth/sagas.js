ca34d1dcc50f719b81bf1142aca3be53daa4eb78
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-31 01:01:51 -0300

    feat: added toastify for errors

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import { signInSuccess, signFailure } from './actions';
@@ -20,6 +21,9 @@ export function* signIn({ payload }) {
 
     history.push('dashboard');
   } catch (err) {
+    toast.error(
+      err.response.data.error || 'Something is wrong... Check your credentials.'
+    );
     yield put(signFailure());
   }
 }
