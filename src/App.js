c9613588cd8ccd4d5bfef64c6b1eea8972c24452
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-31 00:06:52 -0300

    chore: setup redux and sagas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
@@ -7,14 +8,18 @@ import history from './services/history';
 
 import Routes from './routes';
 
+import store from './store';
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
